6f78c1ae241267cab266c8694015d4120c80d25a
Tiberius Mairura <tiberiusmairura90@gmail.com>
2022-05-28 18:54:58 +0300

    fix code indentation

--- todo-app/assets/js/app.js
+++ todo-app/assets/js/app.js
@@ -1,12 +1,9 @@
-
 // Grab the input element
 const inputElem = document.querySelector(".input");
 // Grab the todo ul
 const todoContainer = document.querySelector(".todo-list");
 // Grab the submit button
 const submitBtn = document.querySelector(".add-todo");
-
-
 
 // submit button default css settings
 submitBtn.style.border = "none";
@@ -39,34 +36,31 @@ const addTodoItem = (todoItem) => {
   newLi.textContent = todoItem;
 
   // Create span to hold action buttons
-  let controlContainer = document.createElement('span'); //<span></span>
-  
+  let controlContainer = document.createElement("span"); //<span></span>
+
   // Create the delete and edit buttons and checkbox
-  let deletBtn = document.createElement('button');
+  let deletBtn = document.createElement("button");
   deletBtn.textContent = "Delete";
-  deletBtn.addEventListener('click', deleteTodoItem);
-  deletBtn.classList.add('bg-danger');
+  deletBtn.addEventListener("click", deleteTodoItem);
+  deletBtn.classList.add("bg-danger");
 
-
-  let editBtn = document.createElement('button');
+  let editBtn = document.createElement("button");
   editBtn.textContent = "Edit";
-  editBtn.classList.add('bg-green');
+  editBtn.classList.add("bg-green");
 
-  let check = document.createElement('input');
-  check.addEventListener('click', markAsDone);
-  check.type = 'checkbox'
-  check.classList.add('check')
-
+  let check = document.createElement("input");
+  check.addEventListener("click", markAsDone);
+  check.type = "checkbox";
+  check.classList.add("check");
 
   controlContainer.appendChild(editBtn);
   controlContainer.appendChild(deletBtn);
-  controlContainer.appendChild(check)
+  controlContainer.appendChild(check);
 
-  newLi.appendChild(controlContainer)
-
+  newLi.appendChild(controlContainer);
 
   todoContainer.appendChild(newLi);
-    // todoContainer.innerHTML = `<li>${todoItem}</li>`;
+  // todoContainer.innerHTML = `<li>${todoItem}</li>`;
 };
 
 // Listens for click events and invokes addTodoItem()
@@ -75,25 +69,21 @@ submitBtn.addEventListener("click", () => {
   addTodoItem(userInput);
 });
 
-
-
 const deleteTodoItem = (e) => {
   let targetBtn = e.target;
   let parent = targetBtn.parentNode.parentNode;
-  parent.parentNode.removeChild(parent)  
-}
-
-
-
+  parent.parentNode.removeChild(parent);
+};
 
 const markAsDone = (e) => {
   let targetBtn = e.target;
   let parent = targetBtn.parentNode;
-  if (!parent.parentNode.classList.contains('line-through') && targetBtn.checked) {
-    parent.parentNode.classList.add('line-through');
+  if (
+    !parent.parentNode.classList.contains("line-through") &&
+    targetBtn.checked
+  ) {
+    parent.parentNode.classList.add("line-through");
   } else {
-    parent.parentNode.classList.remove('line-through');
+    parent.parentNode.classList.remove("line-through");
   }
-}
-
-
+};
